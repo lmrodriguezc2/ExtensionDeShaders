feab8eec1da1dbb19bfb679e4554d73c955bcece
unknown <lm.rodriguezc2@uniandes.edu.co>
2021-05-10 22:24:02 -0500

    Escena completa con modificación de shaders y geometría

---
+++ Shaders/Assets/NotasScript.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotasScript : MonoBehaviour
+{
+	public int cantidadPorNota;
+
+	float deltax = 0.01f;
+	float deltay;
+	float sen1;
+	float sen2 = 0;
+	float transformation = 1;
+	float actualx = 0;
+	Vector3 mov;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameObject objetoBase = GameObject.Find("Base");
+		for(int i = 0; i < cantidadPorNota; i++)
+        {
+			GameObject nt = MakeNota1(i);
+			nt.transform.Translate(notColliding());
+			nt.transform.Rotate(0.0f, Random.Range(0.0f, 359.0f), 0.0f, Space.Self);
+			nt.transform.parent = objetoBase.transform;
+		}
+		for (int i = 0; i < cantidadPorNota; i++)
+		{
+			GameObject nt = MakeNota2(i);
+			nt.transform.Translate(notColliding());
+			nt.transform.Rotate(0.0f, Random.Range(0.0f, 359.0f), 0.0f, Space.Self);
+			nt.transform.parent = objetoBase.transform;
+		}
+	}
+
+    private void Update()
+	{
+		GameObject objetoBase = GameObject.Find("Base");
+		foreach (Transform child in objetoBase.transform)
+		{
+			sen1 = sen2;
+			sen2 = Mathf.Sin(Mathf.Deg2Rad * actualx * transformation);
+			mov = new Vector3(deltax, (sen2 - sen1) * 9.0f, 0);
+			child.Translate(mov);
+			child.position = outOfBounds(child.position);
+			actualx += deltax * 20;
+			if (actualx == 360)
+				actualx = 0;
+		}
+	}
+
+    GameObject MakeNota1(int numero)
+	{
+		GameObject Nota1 = new GameObject("NotaG1-" + numero);
+		GameObject Nota10 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Nota10.transform.SetParent(Nota1.transform);
+		Nota10.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+		Nota10.transform.localScale = new Vector3(0.01288273f, 0.09791894f, 0.01065468f);
+		Nota10.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota11 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Nota11.transform.SetParent(Nota1.transform);
+		Nota11.transform.position = new Vector3(0.0f, 0.0f, 0.076f);
+		Nota11.transform.localScale = new Vector3(0.01288273f, 0.09791894f, 0.01065468f);
+		Nota11.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota12 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Nota12.transform.SetParent(Nota1.transform);
+		Nota12.transform.position = new Vector3(0.0f, 0.046f, 0.0384f);
+		Nota12.transform.rotation *= Quaternion.Euler(90, -1.525879e-05f, -1.525879e-05f);
+		Nota12.transform.localScale = new Vector3(0.01288273f, 0.08640555f, 0.01065468f);
+		Nota12.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota13 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Nota13.transform.SetParent(Nota1.transform);
+		Nota13.transform.position = new Vector3(0.0026688f, -0.0587f, -0.0091f);
+		Nota13.transform.rotation *= Quaternion.Euler(31.436f, 15.413f, 8.682f);
+		Nota13.transform.localScale = new Vector3(0.03016089f, 0.04271687f, 0.03016089f);
+		Nota13.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota14 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Nota14.transform.SetParent(Nota1.transform);
+		Nota14.transform.position = new Vector3(0.0037688f, -0.058f, 0.064f);
+		Nota14.transform.rotation *= Quaternion.Euler(31.436f, 15.413f, 8.682f);
+		Nota14.transform.localScale = new Vector3(0.03016089f, 0.04271687f, 0.03016089f);
+		Nota14.GetComponent<Renderer>().material.color = Color.red;
+
+		return Nota1;
+	}
+
+	GameObject MakeNota2(int numero)
+	{
+		GameObject Nota2 = new GameObject("NotaG2-" + numero);
+
+		GameObject Nota20 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Nota20.transform.SetParent(Nota2.transform);
+		Nota20.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+		Nota20.transform.localScale = new Vector3(0.01288273f, 0.09791894f, 0.01065468f);
+		Nota20.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota21 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Nota21.transform.SetParent(Nota2.transform);
+		Nota21.transform.position = new Vector3(-0.00000001f, 0.0402f, 0.0171f);
+		Nota21.transform.rotation *= Quaternion.Euler(101.614f, -1.525879e-05f, -1.525879e-05f);
+		Nota21.transform.localScale = new Vector3(0.01288273f, 0.04592196f, 0.01065468f);
+		Nota21.GetComponent<Renderer>().material.color = Color.red;
+
+		GameObject Nota22 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Nota22.transform.SetParent(Nota2.transform);
+		Nota22.transform.position = new Vector3(0.0026688f, -0.0587f, -0.0091f);
+		Nota22.transform.rotation *= Quaternion.Euler(31.436f, 15.413f, 8.682f);
+		Nota22.transform.localScale = new Vector3(0.03016089f, 0.04271687f, 0.03016089f);
+		Nota22.GetComponent<Renderer>().material.color = Color.red;
+
+		return Nota2;
+	}
+
+	Vector3 notColliding()
+    {
+		float x = Random.Range(-3.4f, 3.1f);
+		float y = Random.Range(0.0f, 3.0f);
+		float z = Random.Range(-3.7f, 3.6f);
+		while (x < 1.4f && x > -0.5f && y < 1.4f && y > 0.0f && z < 1.2f && z > -1.0f)
+        {
+			x = Random.Range(-3.4f, 3.1f);
+			y = Random.Range(0.0f, 3.0f);
+			z = Random.Range(-3.7f, 3.6f);
+		}
+		return new Vector3(x, y, z);
+	}
+
+	Vector3 outOfBounds(Vector3 inp)
+    {
+		if (inp.x < -3.4f)
+			inp.x = 3.1f;
+		if (inp.x > 3.1f)
+			inp.x = -3.4f;
+		if (inp.z < -3.7f)
+			inp.z = 3.6f;
+		if (inp.z > 3.6f)
+			inp.z = -3.7f;
+		return inp;
+	}
+}
---
+++ Shaders/Assets/Shaders/TrianglePyramidExtrusion.shader
@@ -0,0 +1,124 @@
+﻿Shader "Geometry/TrianglePyramidExtrusion"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_ExtrusionFactor("Extrusion factor", float) = 0.5
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		Cull Off
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma geometry geom
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float3 normal : NORMAL;
+			};
+
+			struct v2g
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float3 normal : NORMAL;
+			};
+
+			struct g2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _ExtrusionFactor;
+
+			v2g vert(appdata v)
+			{
+				v2g o;
+				o.vertex = v.vertex;
+				o.uv = v.uv;
+				o.normal = v.normal;
+				return o;
+			}
+
+			float random(float2 uv)
+			{
+				return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453123);
+			}
+
+			[maxvertexcount(12)]
+			void geom(triangle v2g IN[3], inout TriangleStream<g2f> triStream)
+			{
+				g2f o;
+
+				float4 barycenter = (IN[0].vertex + IN[1].vertex + IN[2].vertex) / 3;
+				float3 normal = (IN[0].normal + IN[1].normal + IN[2].normal) / 3;
+
+				for (int i = 0; i < 3; i++) {
+					int next = (i + 1) % 3;
+
+					o.vertex = UnityObjectToClipPos(IN[i].vertex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					o.uv = TRANSFORM_TEX(IN[i].uv, _MainTex);
+					o.color = fixed4(0.0, 0.0, 0.0, 1.0);
+					triStream.Append(o);
+
+					o.vertex = UnityObjectToClipPos(barycenter + float4(normal, 0.0) * _ExtrusionFactor);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					o.uv = TRANSFORM_TEX(IN[i].uv, _MainTex);
+					float rnd1 = random(float2(o.vertex[0], o.vertex[1]));
+					float rnd2 = random(float2(o.vertex[1], o.vertex[2]));
+					float rnd3 = random(float2(o.vertex[2], o.vertex[0]));
+					o.color = float4(rnd1, rnd2, rnd3, 1.0);
+					triStream.Append(o);
+
+					o.vertex = UnityObjectToClipPos(IN[next].vertex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					o.uv = TRANSFORM_TEX(IN[next].uv, _MainTex);
+					o.color = fixed4(0.0, 0.0, 0.0, 1.0);
+					triStream.Append(o);
+
+					triStream.RestartStrip();
+				}
+
+				for (int j = 0; j < 3; j++)
+				{
+					o.vertex = UnityObjectToClipPos(IN[j].vertex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					o.uv = TRANSFORM_TEX(IN[j].uv, _MainTex);
+					o.color = fixed4(0.0, 0.0, 0.0, 1.0);
+					triStream.Append(o);
+				}
+
+				triStream.RestartStrip();
+			}
+
+			fixed4 frag(g2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv) * i.color;
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+
+			ENDCG
+		}
+	}
+}
